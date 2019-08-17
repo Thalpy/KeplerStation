b8ebf380d48ade146a91d49cd32d314cadfb2d77
AffectedArc07 <josephcase55@hotmail.com>
2019-08-17 17:52:24 +0100

    This is why you dont copypaste

--- tools/travis/install_libs.sh
+++ tools/travis/install_libs.sh
@@ -7,5 +7,5 @@ mkdir -p ~/.byond/bin
 wget -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x ~/.byond/bin/librust_g.so
 
-wget -O ~/.byond/bin/libBSQL.so "https://github.com/tgstation/rust-g/releases/download/$BSQL_VERSION/libBSQL.so"
+wget -O ~/.byond/bin/libBSQL.so "https://github.com/tgstation/BSQL/releases/download/$BSQL_VERSION/libBSQL.so"
 chmod +x ~/.byond/bin/libBSQL.so
